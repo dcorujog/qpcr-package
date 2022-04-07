a354080b5dc03e39f6c8916ba3f15e240dba20d7
David Corujo <dcorujog@gmail.com>
2022-04-07 11:49:57 +0200

    Write documentation for norm_factor_fun

--- R/norm_factor_fun.R
+++ R/norm_factor_fun.R
@@ -1,3 +1,16 @@
+#' Calculate normalization factor
+#'
+#' @param dct_data data.frame containing the qPCR data including dCt values as calculated by \code{\link{dct_apply}}
+#' @param hkg a character vector containing the names of the targets used as normalization genes
+#' @param exp_name experiment name (used as plot title and to name the saved file)
+#'
+#' @import ggplot2
+#'
+#' @return a named numeric vector with the normalization factor calculated for each sample. In addition, saves
+#' a PDF containing a scatterplot to asses the correlation between the expression values of normalization genes (if using more than one)
+#' @export
+#'
+#' @examples
 norm_factor_fun <- function(dct_data, hkg, exp_name) { # calculate normalization factor for each sample and generate a scatterplot of housekeeping gene expression
   hkg_data <- dct_data[grep(paste(hkg, collapse = "|"), dct_data$primers),]
   hkg_centered <- aggregate(hkg_data$dct, by = hkg_data["primers"], FUN = function(x) x)
