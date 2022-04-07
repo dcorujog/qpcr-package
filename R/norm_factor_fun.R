349f171ed5359fa93ab34c3162a4544e89b47c4e
David Corujo <dcorujog@gmail.com>
2022-04-07 15:16:49 +0200

    Fixes #1

--- R/norm_factor_fun.R
+++ R/norm_factor_fun.R
@@ -21,20 +21,22 @@ norm_factor_fun <- function(dct_data, hkg, exp_name) { # calculate normalization
   rownames(hkg_centered) <- unique(hkg_data$sample)
   norm_factor <- apply(hkg_centered, 1, mean, na.rm = T)
 
+  if (length(hkg) > 1) { # Skip if using only one normalization gene
   # Scatter plot of HK gene expression values
-  hkg_centered <- as.data.frame(hkg_centered)
-  hkg_plot <- ggplot(hkg_centered, aes(x = get(hkg[1]), y = get(hkg[2]), label = rownames(hkg_centered))) +
-    coord_cartesian(xlim = c(min(hkg_centered), max(hkg_centered)),
-                    ylim = c(min(hkg_centered), max(hkg_centered))) +
-    geom_abline(slope = 1, intercept = 0, color = "darkblue") +
-    geom_point() +
-    geom_text(hjust = 0, nudge_x = 0.05) +
-    labs(x = names(hkg_centered)[1],
-         y = names(hkg_centered)[2])
+    hkg_centered <- as.data.frame(hkg_centered)
+    hkg_plot <- ggplot(hkg_centered, aes(x = get(hkg[1]), y = get(hkg[2]), label = rownames(hkg_centered))) +
+      coord_cartesian(xlim = c(min(hkg_centered), max(hkg_centered)),
+                      ylim = c(min(hkg_centered), max(hkg_centered))) +
+      geom_abline(slope = 1, intercept = 0, color = "darkblue") +
+      geom_point() +
+      geom_text(hjust = 0, nudge_x = 0.05) +
+      labs(x = names(hkg_centered)[1],
+           y = names(hkg_centered)[2])
 
-  ggsave(filename = paste0(exp_name, "_hkg_scatter.pdf"),
-         plot = hkg_plot,
-         device = cairo_pdf)
+    ggsave(filename = paste0(exp_name, "_hkg_scatter.pdf"),
+           plot = hkg_plot,
+           device = cairo_pdf)
+  }
 
   norm_factor
 }
