959fc99e4dd5a3788d15b2dc67a39dce6939c51a
David Corujo <dcorujog@gmail.com>
2022-04-07 10:29:05 +0200

    Write documentation for ct_sd_plot

--- R/ct_sd_plot.R
+++ R/ct_sd_plot.R
@@ -1,3 +1,17 @@
+#' QC plot of Ct standard deviation of technical replicates
+#'
+#' @param norm_data data frame with the normalized qPCR data as generated bu the \code{\link{qpcr_analysis}}
+#' @param max_sd maximum standard deviation value between technical replicates to be considered acceptable, a horizontal red line will be drawn in the plot at this value (default = sd(c(0, 0.5)))
+#' @param max_ct maximum Ct value considered for quantitative range, a vertical red line will be drawn in the plot at this value (default = 35)
+#' @param exp_name experiment name (used as plot title and to name the saved file)
+#'
+#' @import ggplot2
+#' @import viridis
+#'
+#' @return
+#' @export
+#'
+#' @examples
 ct_sd_plot <- function(norm_data, max_sd = sd(c(0, 0.5)), max_ct = 35, exp_name = "exp_name") {
   ctsd <- ggplot(norm_data, aes(x = ctaverage, y = ct_sd, color = primers, fill = primers)) +
     geom_point(alpha = 0.8) +
