43b90696beffff4addd74283548788e51933ef28
David Corujo <dcorujog@gmail.com>
2022-04-07 10:24:41 +0200

    Write documentation for ct_avg_plot

--- R/ct_avg_plot.R
+++ R/ct_avg_plot.R
@@ -1,3 +1,16 @@
+#' QC plot of Ct averages of technical replicates
+#'
+#' @param norm_data data frame with the normalized qPCR data as generated bu the \code{\link{qpcr_analysis}}
+#' @param max_ct maximum Ct value considered for quantitative range, a horizontal red line will be drawn in the plot at this value (default = 35)
+#' @param exp_name experiment name (used as plot title and to name the saved file)
+#'
+#' @import ggplot2
+#' @import viridis
+#'
+#' @return a pdf file is saved with the corresponding plot
+#' @export
+#'
+#' @examples
 ct_avg_plot <- function(norm_data, max_ct = 35, exp_name = "exp_name") {
   ctavg <- ggplot(norm_data, aes(x = primers, y = ctaverage, color = primers, group = primers, size = ct_sd)) +
     geom_jitter(width = 0.2, alpha = 0.75) +
