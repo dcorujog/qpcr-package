70d3cde8080a84c21435d4e45194ace4bb72205c
David Corujo <dcorujog@gmail.com>
2022-05-10 14:56:08 +0200

    Remove viridis scales

--- R/ct_sd_plot.R
+++ R/ct_sd_plot.R
@@ -16,8 +16,8 @@ ct_sd_plot <- function(norm_data, max_sd = sd(c(0, 0.5)), max_ct = 35, exp_name 
   ctsd <- ggplot(norm_data, aes(x = ctaverage, y = ct_sd, color = primers, fill = primers)) +
     geom_point(alpha = 0.8) +
     geom_hline(yintercept = max_sd, color = "red") +
-    geom_vline(xintercept = max_ct, color = "red") +
-    scale_color_viridis(option = "turbo", discrete = TRUE)
+    geom_vline(xintercept = max_ct, color = "red") #+
+    #scale_color_viridis(option = "turbo", discrete = TRUE)
   pdf(paste0(exp_name, "_ct_sd_plot.pdf"))
   print(ctsd)
   dev.off()
