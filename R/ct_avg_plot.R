f85e48981b5606e291b6e5cb33e62d841c45dc85
David Corujo <dcorujog@gmail.com>
2022-05-18 16:29:15 +0200

    Add viridis scales

--- R/ct_avg_plot.R
+++ R/ct_avg_plot.R
@@ -15,7 +15,7 @@ ct_avg_plot <- function(norm_data, max_ct = 35, exp_name = "exp_name") {
   ctavg <- ggplot(norm_data, aes(x = primers, y = ctaverage, color = primers, group = primers, size = ct_sd)) +
     geom_jitter(width = 0.2, alpha = 0.75) +
     geom_hline(yintercept = max_ct, color = "red") +
-    #scale_color_viridis(option = "turbo", discrete = TRUE) +
+    scale_color_viridis(option = "turbo", discrete = TRUE) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
   pdf(paste0(exp_name, "_ct_avg_plot.pdf"))
   print(ctavg)
--- R/eff_avg_plot.R
+++ R/eff_avg_plot.R
@@ -16,8 +16,8 @@
 eff_avg_plot <- function(norm_data, effrange = c(1.8, 2), exp_name = "exp_name") {
   effavg <- ggplot(norm_data, aes(x = primers, y = effaverage, color = ctaverage, group = primers)) +
     geom_point(alpha = 0.75) +
-    geom_hline(yintercept = effrange, color = "red") #+
-    # scale_color_viridis(option = "turbo")
+    geom_hline(yintercept = effrange, color = "red") +
+    scale_color_viridis(option = "turbo")
   pdf(paste0(exp_name, "_eff_avg_plot.pdf"))
   print(effavg)
   dev.off()
